8741c725c4fa70822bd059c5b989de7da03f828c
Henry Darnell <hjdarnel@users.noreply.github.com>
2020-02-07 14:27:01 -0600

    Modify Control+` to CMD+\

--- keyboards/hhkb/keymaps/hjdarnel/keymap.c
+++ keyboards/hhkb/keymaps/hjdarnel/keymap.c
@@ -7,6 +7,7 @@
 #define HHKB 1
 #define _______ KC_TRNS
 #define KC_CTLG LCTL(KC_GRV)
+#define KC_CTLS LGUI(KC_BSLASH)
 #define KC_CTLC LCTL(KC_C)
 #define KC_CTLU LCTL(KC_U)
 #define KC_CTCU LGUI(LCTL(KC_V))
@@ -68,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         /* Layer 2: Extra stuff (Control key)
       |------+-----+-----+-----+--------+----+ CSh^+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |     |     |     |        |    |    |    |     |     | Mut | VoD | VoU   |       | Ctl`|
+      |      |     |     |     |        |    |    |    |     |     | Mut | VoD | VoU   |       | Cmd\|
       |------+-----+-----+-----+--------+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       |      | VoU |     |     |        |    |    |    | CtlU|     |     |     |       |       |     |
       |------+-----+-----+-----+--------+----+----+----+-----+-----+-----+-----+-------+-------+-----|
@@ -84,8 +85,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [2] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, KC_CSIX, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_CTLG,
-        _______, KC_VOLU, _______, _______, _______, _______, _______, KC_CTLU, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_CSIX, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_CTLS,
+        _______, KC_VOLU, _______, _______, _______, _______, _______, KC_CTLU, _______, _______, _______, _______, _______, KC_DEL,
         _______, KC_MEDIA_REWIND, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_FAST_FORWARD, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, MY_CUSTOM_MACRO,
         _______, KC_VOLD, _______, KC_CTLC, KC_CTCU, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______)};
